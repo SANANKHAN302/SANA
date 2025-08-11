b7e623b699be836ebb9655281b831f37d60bd458
SANANKHAN302 <143243818+SANANKHAN302@users.noreply.github.com>
2025-08-11 08:56:44 +0500

    Update admin_det.js

--- Priyansh/commands/admin_det.js
+++ Priyansh/commands/admin_det.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   if (event.body.indexOf("ADMIN")==0 || event.body.indexOf("Admin")==0 || event.body.indexOf("/Admin")==0 || event.body.indexOf("#admin")==0) {
     var msg = {
-        body: "🫅 𝐎𝐖𝐍𝐄𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 🫅",
+        body: "🫅  🫅",
         attachment: 
 fs.createReadStream(__dirname + `/noprefix/profile.png`)
       }
